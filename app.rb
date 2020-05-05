ee61b63b330e580f3bef802c72ec7be68b68944b
Gor <7495500@gmail.com>
2020-05-05 18:21:42 +0300

    Add rakefile and model

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, "sqlite3:pizzashop.sqlite"
+
+class Product < Activerecord::Base
+end
 
 get '/' do
 	erb :index
